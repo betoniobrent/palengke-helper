25cb86f3c66e18f97176522e7fad11e135dda368
betoniobrent <155439539+betoniobrent@users.noreply.github.com>
2026-07-21 15:12:32 +0800

    supabase

---
+++ supabase.js
@@ -0,0 +1,8 @@
+// supabase.js
+const SUPABASE_URL = 'https://hurqvygjmaamrynuptuo.supabase.co';
+const SUPABASE_PUBLIC_KEY = 'sb_publishable_5uEs9q8sYfr8fR9OBZGxXA_c6nQ2QGD';
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLIC_KEY
+);
